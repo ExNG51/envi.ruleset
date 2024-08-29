355fdaffea96394edde0a32e26bcc0b669ebbd15
ExNG51 <oxj2c@tuta.io>
2024-08-29 23:49:16 +0800

    chore(vps-setup): update alma_setup.sh

--- vps/alma_setup.sh
+++ vps/alma_setup.sh
@@ -3,11 +3,38 @@
 # 更新软件包
 dnf update -y
 
+# 开启swap增加交换空间
+SWAP_SIZE=$((4 * 1024 * 1024))  # 设置最大交换空间为 4GB
+dd if=/dev/zero of=/mnt/swap bs=1M count=$SWAP_SIZE
+chmod 600 /mnt/swap
+mkswap /mnt/swap
+echo "/mnt/swap swap swap defaults 0 0" >> /etc/fstab
+sed -i '/vm.swappiness/d' /etc/sysctl.conf
+echo "vm.swappiness = 25" >> /etc/sysctl.conf
+sysctl -w vm.swappiness=25
+swapon -a
+
+# 安装tuned
+dnf install tuned -y && systemctl enable --now tuned
+
+# 安装Docker和Docker Compose
+dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+dnf install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+systemctl enable --now docker
+
+# 安装Node.js和npm
+curl -fsSL https://rpm.nodesource.com/setup_19.x | bash -
+dnf install -y nodejs
+
+# 安装python
+curl https://mise.run | sh
+mise use -g python@3.10
+
 # 修改时区为上海
 timedatectl set-timezone Asia/Shanghai
 
 # 修改SSH端口
-sed -i 's/#Port 22/Port 9399/' /etc/ssh/sshd_config
+sed -i 's/#Port 22/Port 9929/' /etc/ssh/sshd_config
 systemctl restart sshd
 
 # 运行NextTrace安装脚本
