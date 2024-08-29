fed11e62eb060373b170dde3b4d4731c8b9d9ef3
ExNG51 <oxj2c@tuta.io>
2024-08-29 23:43:12 +0800

    Update alma_setup.sh

--- vps/alma_setup.sh
+++ vps/alma_setup.sh
@@ -3,24 +3,6 @@
 # 更新软件包
 dnf update -y
 
-# 修改时区为上海
-timedatectl set-timezone Asia/Shanghai
-
-# 修改SSH端口
-sed -i 's/#Port 22/Port 9399/' /etc/ssh/sshd_config
-systemctl restart sshd
-
-# 运行NextTrace安装脚本
-bash -c "$(curl -Ls https://github.com/sjlleo/nexttrace/raw/main/nt_install.sh)"
-
-# 设置BBR+FQ
-echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
-sysctl -p
-
-# 安装dnsutils
-dnf install -y bind-utils
-
 # 开启swap增加交换空间
 dd if=/dev/zero of=/mnt/swap bs=1M count=`awk '($1 == "MemTotal:"){print int($2/1024*2)}' /proc/meminfo`
 chmod 600 /mnt/swap
@@ -46,5 +28,23 @@ sudo dnf install -y nodejs
 # 安装python
 curl https://mise.run | sh
 mise use -g python@3.10
+
+# 修改时区为上海
+timedatectl set-timezone Asia/Shanghai
+
+# 修改SSH端口
+sed -i 's/#Port 22/Port 9399/' /etc/ssh/sshd_config
+systemctl restart sshd
+
+# 运行NextTrace安装脚本
+bash -c "$(curl -Ls https://github.com/sjlleo/nexttrace/raw/main/nt_install.sh)"
+
+# 设置BBR+FQ
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+sysctl -p
+
+# 安装dnsutils
+dnf install -y bind-utils
 
 echo "所有任务完成！"
