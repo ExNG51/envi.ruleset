ae2c2c5da64cef7034c5baad1d7cd5c0680b983b
ExNG51 <oxj2c@tuta.io>
2024-08-31 01:46:26 +0800

    chore(vps-setup): update almalinux_setup.sh

--- vps/almalinux_setup.sh
+++ vps/almalinux_setup.sh
@@ -119,7 +119,7 @@ tuned-adm profile network-throughput
 # 安装 Docker
 echo "安装 Docker..."
 sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo systemctl start docker
 sudo systemctl enable docker
 docker --version
