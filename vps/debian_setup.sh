7588cd162b48b7843b982f4f3f84f67ac4ca9f63
ExNG51 <oxj2c@tuta.io>
2024-08-31 00:23:08 +0800

    chore(vps-setup): update debian_setup.sh

--- vps/debian_setup.sh
+++ vps/debian_setup.sh
@@ -6,20 +6,6 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-# 检测并安装所需的指令
-check_and_install() {
-    if ! command -v $1 &> /dev/null; then
-        echo "$1 未安装，正在安装..."
-        apt-get install -y $1
-        if [ $? -ne 0 ]; then
-            echo "$1 安装失败。"
-            exit 1
-        fi
-    else
-        echo "$1 已安装，跳过安装。"
-    fi
-}
-
 # 更新系统
 echo "更新系统..."
 apt-get update -o Acquire::ForceIPv4=true && apt-get full-upgrade -y
@@ -36,35 +22,7 @@ check_and_install dnsutils
 # 修改时区为新加坡
 sudo timedatectl set-timezone Asia/Singapore
 
-# 修改 SSH 端口为 9399
-echo "修改 SSH 端口为 9399..."
-sed -i 's/#Port 22/Port 9399/' /etc/ssh/sshd_config
-systemctl restart sshd
-if [ $? -ne 0 ]; then
-    echo "SSH 服务重启失败，请检查配置。"
-    exit 1
-fi
-
-# 开启 TCP Fast Open (TFO)
-echo "开启 TCP Fast Open (TFO)..."
-echo "3" > /proc/sys/net/ipv4/tcp_fastopen
-echo "net.ipv4.tcp_fastopen=3" > /etc/sysctl.d/30-tcp_fastopen.conf
-sysctl --system
-
-# 开启 tuned 并设置网络性能优化
-echo "开启 tuned 并设置网络性能优化配置..."
-check_and_install tuned
-systemctl enable tuned.service
-systemctl start tuned.service
-tuned-adm profile network-throughput
-
-# 卸载 tcp-brutal 模块
-dkms uninstall tcp-brutal/1.0.1 --all && dkms remove tcp-brutal/1.0.1 --all
-
-# 检查 dkms 状态
-dkms status
-
-# 在 /root 目录下创建 kernel 文件夹并进入
+# 创建 /root/kernel 目录并进入
 echo "创建 /root/kernel 目录并进入..."
 mkdir -p /root/kernel && cd /root/kernel
 
@@ -89,6 +47,12 @@ dpkg -i linux-image-*-egoist-cloud_*.deb
 # 清理下载的deb包
 rm -f *.deb
 
+# 开启 TCP Fast Open (TFO)
+echo "开启 TCP Fast Open (TFO)..."
+echo "3" > /proc/sys/net/ipv4/tcp_fastopen
+echo "net.ipv4.tcp_fastopen=3" > /etc/sysctl.d/30-tcp_fastopen.conf
+sysctl --system
+
 # 设置 bbrv3
 echo 'net.ipv4.tcp_congestion_control=bbr' | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p
@@ -96,9 +60,18 @@ sudo sysctl -p
 # 查看当前的TCP流控算法
 sysctl net.ipv4.tcp_congestion_control
 
-# 安装路由测试工具
-echo "安装路由测试工具 nexttrace..."
-bash -c "$(curl -Ls https://github.com/sjlleo/nexttrace/raw/main/nt_install.sh)"
+# 开启 tuned 并设置网络性能优化配置
+echo "开启 tuned 并设置网络性能优化配置..."
+check_and_install tuned
+systemctl enable tuned.service
+systemctl start tuned.service
+tuned-adm profile network-throughput
+
+# 卸载 tcp-brutal 模块
+dkms uninstall tcp-brutal/1.0.1 --all && dkms remove tcp-brutal/1.0.1 --all
+
+# 检查 dkms 状态
+dkms status
 
 # 安装 Docker
 echo "安装 Docker..."
@@ -108,5 +81,18 @@ curl -fSL https://get.docker.com | bash -s docker
 echo "安装 Node.js 19.x..."
 curl -fsSL https://deb.nodesource.com/setup_19.x | bash -
 apt-get install -y nodejs
+
+# 修改 SSH 端口为 9399
+echo "修改 SSH 端口为 9399..."
+sed -i 's/#Port 22/Port 9399/' /etc/ssh/sshd_config
+systemctl restart sshd
+if [ $? -ne 0 ]; then
+    echo "SSH 服务重启失败，请检查配置。"
+    exit 1
+fi
+
+# 安装路由测试工具
+echo "安装路由测试工具 nexttrace..."
+bash -c "$(curl -Ls https://github.com/sjlleo/nexttrace/raw/main/nt_install.sh)"
 
 echo "所有步骤完成！"
